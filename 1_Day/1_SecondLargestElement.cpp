f0f1cd9b12b7a78c376a232cfdeb656a627e50a1
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2023-07-29 16:52:45 +0530

    addded

--- 1_Day/1_SecondLargestElement.cpp
+++ 1_Day/1_SecondLargestElement.cpp
@@ -1,23 +1,23 @@
-class Solution{
-public:	
-	// Function returns the second
-	// largest elements
-	int print2largest(int arr[], int n) {
-	    // code here
-	   int firstLargest=INT_MIN;
+// class Solution{
+// public:	
+// 	// Function returns the second
+// 	// largest elements
+// 	int print2largest(int arr[], int n) {
+// 	    // code here
+// 	   int firstLargest=INT_MIN;
 	   
-	   for(int i=0;i<n;i++){
-	       if(arr[i]>firstLargest){
-	           firstLargest=arr[i];
-	       }
-	   }
+// 	   for(int i=0;i<n;i++){
+// 	       if(arr[i]>firstLargest){
+// 	           firstLargest=arr[i];
+// 	       }
+// 	   }
 	   
-	   int secondLargest=-1;
-	   for(int i=0;i<n;i++){
-	       if(arr[i]>secondLargest && arr[i]<firstLargest){
-	           secondLargest=arr[i];
-	       }
-	   }
-	   return secondLargest;
-	}
-};
+// 	   int secondLargest=-1;
+// 	   for(int i=0;i<n;i++){
+// 	       if(arr[i]>secondLargest && arr[i]<firstLargest){
+// 	           secondLargest=arr[i];
+// 	       }
+// 	   }
+// 	   return secondLargest;
+// 	}
+// };
