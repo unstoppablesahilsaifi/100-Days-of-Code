1396a983458da56782635ac38c62fe8eceda6dc5
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2023-08-04 16:32:51 +0530

    added

---
+++ 4_Day/1_Beautiful_String.cpp
@@ -0,0 +1,21 @@
+// int makeBeautiful(string str) {
+// 	// Write your code here
+// 	string copy=str;
+
+
+//     int a=0,b=0;
+
+//     for(int i=0;i<str.size();i++){
+
+//         if(i&1 and str[i]=='0') a++; 
+
+//         if(i%2==0 and str[i]=='1') a++; 
+
+//         if(i&1 and str[i]=='1') b++; 
+
+//         if(i%2==0 and str[i]=='0') b++; 
+
+//     }
+
+//     return min(a,b);
+// }
